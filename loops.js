b9329c320580c6e3505702be6ecad1bf1ee1fb38
Andres Rivera <soulkalir@gmail.com>
2020-08-01 00:48:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,46 +1,16 @@
 
 function forLoop(array){
-  for(let i=0; i<25; i++){
-    if(i===1){
-        array.push("I am 1 strange loop.");
+ for(let i=0; i<25; i++){
+    if(i>1){
+      console.log(`'I am ${i} strange loop.'`)
     }else{
-     array.push(`I am ${i} strange loops.`);
+      console.log(`'I am ${i} strange loops.'`)
     }
-
-
   }
-  
- return array;
-
+  return forLoop(array);
 }
 
-
-function whileLoop(number){
-  while(number>0) {
-    console.log(number)
-    number --;                 
-  }   
-  return 'done'
-}
-
-
-
-function doWhileLoop(integer){
-  
-  let i = 0;
-
-  function incrementVariable() {
-    i = i + 1;
-    return i;
-  }
  
-  do {
-    console.log("I run once regardless");
-  } 
-  while (incrementVariable() < integer);
-  
-  
-}
 
 
 
