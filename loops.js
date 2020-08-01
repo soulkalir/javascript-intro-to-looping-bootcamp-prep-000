649d872174f14c9ab005f64637aa6cdd815614bd
Andres Rivera <soulkalir@gmail.com>
2020-08-01 00:51:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,12 +2,12 @@
 function forLoop(array){
  for(let i=0; i<25; i++){
     if(i>1){
-      console.log(`'I am ${i} strange loop.'`)
+      console.log('I am 1 strange loop.')
     }else{
-      console.log(`'I am ${i} strange loops.'`)
+      console.log(`"I am ${i} strange loops."`)
     }
   }
-  return forLoop(array);
+  return array;
 }
 
  
